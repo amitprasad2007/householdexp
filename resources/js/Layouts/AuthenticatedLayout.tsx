2d2e7cadd918d20f03489f737d22f7d5b19c12ae
Amit Prasad <amitprasad@learningspiral.co.in>
2025-12-06 15:17:52 +0530

    changes

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -15,8 +15,6 @@ export default function Authenticated({
         <div className="min-h-screen bg-gray-100 dark:bg-gray-900 flex">
             {/* Desktop Sidebar */}
             <Sidebar />
-            
-
             {/* Main Content Area */}
             <div className="flex-1 flex flex-col min-h-screen relative">
                 {/* Top Navigation Bar (Mobile Logo + User Dropdown) */}
