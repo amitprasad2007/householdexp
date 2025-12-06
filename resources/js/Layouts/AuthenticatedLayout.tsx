4dda0552432276904b4321e5be5949d7cab54604
Amit Prasad <amitprasad@learningspiral.co.in>
2025-12-06 15:16:14 +0530

    chnage

--- resources/js/Layouts/AuthenticatedLayout.tsx
+++ resources/js/Layouts/AuthenticatedLayout.tsx
@@ -15,6 +15,7 @@ export default function Authenticated({
         <div className="min-h-screen bg-gray-100 dark:bg-gray-900 flex">
             {/* Desktop Sidebar */}
             <Sidebar />
+            
 
             {/* Main Content Area */}
             <div className="flex-1 flex flex-col min-h-screen relative">
